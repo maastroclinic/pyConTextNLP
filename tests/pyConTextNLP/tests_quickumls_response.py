1978598b2408185753b7c92029619f0b249bea47
Sander Puts <sander.puts@maastro.nl>
2018-07-10 17:23:18 +0200

    quickumls test edit

--- tests/pyConTextNLP/tests_quickumls_response.py
+++ tests/pyConTextNLP/tests_quickumls_response.py
@@ -1,47 +1,107 @@
-import pyConTextNLP.pyConTextGraph as pyConText
-import pyConTextNLP.itemData as itemData
-import networkx as nx
 import json
-import pathlib
 import os
+import pathlib
+import unittest
 
-absolute_path_string_abspath = os.path.abspath("..\..\KB\lexical_kb_04292013.tsv")
-print(absolute_path_string_abspath)
+import networkx as nx
+import nltk
+from IPython.display import HTML
+from textblob import TextBlob
 
-modifiers = itemData.instantiateFromCSVtoitemData(pathlib.Path(absolute_path_string_abspath).as_uri())
+import pyConTextNLP.display.html as html
+import pyConTextNLP.itemData as itemData
+import pyConTextNLP.pyConTextGraph as pyConText
 
-data_result = ""
-with open('data_result.txt') as json_file:
-    data_result = json.load(json_file)
+nltk.download('punkt')
+
+path_report = 'report.txt'
+path_targets_quickumls = 'report_quickumls_out.json'
+path_modifiers_kb = "..\..\KB\lexical_kb_05042016_nl.tsv"
+path_result_pycontextnlp_xml = 'report_pycontextnlp_out.xml'
 
 
-targets = itemData.instantiateFromQuickUMLSResponseToitemData(data_result['response'])
+class TestQuickUMLSIntegration(unittest.TestCase):
 
-report = ""
-with open('data.txt', 'r') as reportfile:
-    report = reportfile.read()
+    report = {}
+    modifiers = {}
+    targets = {}
 
-markup = pyConText.ConTextMarkup()
-isinstance(markup, nx.DiGraph)
+    def test_quickumls_to_xml(self):
+        self.load_data_files()
+        context = perform_py_context_nlp(self.report, self.modifiers, self.targets)
+        write_result_xml(context, path_result_pycontextnlp_xml)
 
-markup.setRawText(report)
-print(markup)
-print(len(markup.getRawText()))
+    def load_data_files(self):
+        absolute_path_string_abspath = os.path.abspath(path_modifiers_kb)
+        self.modifiers = itemData.instantiateFromCSVtoitemData(pathlib.Path(absolute_path_string_abspath).as_uri())
 
-markup.cleanText()
-print(markup)
-print(len(markup.getText()))
+        with open(path_targets_quickumls) as json_file:
+            data_result = json.load(json_file)
+        self.targets = itemData.instantiateFromQuickUMLSResponseToitemData(data_result['response'])
 
-markup.markItems(modifiers, mode="modifier")
-print(markup.nodes(data=True))
+        with open(path_report, 'r') as reportfile:
+            self.report = reportfile.read()
 
-markup.markItems(targets, mode="target")
-print(markup.nodes(data=True))
 
-markup.pruneMarks()
-markup.dropMarks('Exclusion')
-# apply modifiers to any targets within the modifiers scope
-markup.applyModifiers()
-markup.pruneSelfModifyingRelationships()
+def perform_py_context_nlp(report, modifiers, targets):
+    context = pyConText.ConTextDocument()
+    blob = TextBlob(report.lower())
+    rslts = []
+    for s in blob.sentences:
+        m = markup_sentence(s.raw, modifiers=modifiers, targets=targets)
+        rslts.append(m)
 
-print(markup)
+    for r in rslts:
+        context.addMarkup(r)
+
+    return context
+
+
+def markup_sentence(s, modifiers, targets, prune_inactive=True):
+    """
+    """
+    markup = pyConText.ConTextMarkup()
+    markup.setRawText(s)
+    markup.cleanText()
+    markup.markItems(modifiers, mode="modifier")
+    markup.markItems(targets, mode="target")
+    markup.pruneMarks()
+    markup.dropMarks('Exclusion')
+    # apply modifiers to any targets within the modifiers scope
+    markup.applyModifiers()
+    markup.pruneSelfModifyingRelationships()
+    if prune_inactive:
+        markup.dropInactiveModifiers()
+    return markup
+
+
+def write_result_html(context, path):
+    def get_colors():
+        return {
+            "bowel_obstruction": "blue",
+            "inflammation": "blue",
+            "definite_negated_existence": "red",
+            "probable_negated_existence": "indianred",
+            "ambivalent_existence": "orange",
+            "probable_existence": "forestgreen",
+            "definite_existence": "green",
+            "historical": "goldenrod",
+            "indication": "pink",
+            "acute": "golden"
+        }
+
+    # hmtl bug? AttributeError: 'NodeView' object has no attribute 'sort'
+    data = HTML(html.mark_document_with_html(context, colors=get_colors(), default_color="black"))
+    print('writing pycontextnlp html result to file:', path)
+    with open(path, 'w') as outfile:
+        outfile.write(data)
+
+
+def write_result_xml(context, path):
+    print('writing pycontextnlp xml result to file:', path)
+    with open(path, 'w') as outfile:
+        outfile.write(context.getXML())
+
+
+if __name__ == '__main__':
+    unittest.main()
