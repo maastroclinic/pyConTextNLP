03219eb28cfdbcd901af791de583f129b8c3aeb6
Sander Puts <putssander@gmail.com>
2018-10-19 13:17:58 +0200

    moved code to utils to prevent duplication

--- pyConTextNLP/utils.py
+++ pyConTextNLP/utils.py
@@ -1,4 +1,7 @@
 """ """
+import pyConTextNLP.pyConText as pyConText
+from textblob import TextBlob
+
 
 def get_document_markups(document):
     """ Given a ConTextDocument return an ordered list of the ConTextmarkup objects consistituting the document"""
@@ -17,3 +20,36 @@ def get_section_markups(document, sectionLabel):
 def conceptInDocument(document, concept):
     """tests whether concept is in any nodes of document"""
     pass
+
+def perform_py_context_nlp(modifiers, targets, document):
+    """"""
+    blob = TextBlob(document.lower())
+    rslts = []
+    for s in blob.sentences:
+        m = markup_sentence(s.raw, modifiers=modifiers, targets=targets)
+        rslts.append(m)
+
+    return rslts
+
+def markup_sentence(s, modifiers, targets, prune_inactive=True):
+    """
+    """
+    markup = pyConText.ConTextMarkup()
+    markup.setRawText(s)
+    markup.cleanText()
+    markup.markItems(modifiers, mode="modifier")
+    markup.markItems(targets, mode="target")
+    markup.pruneMarks()
+    markup.dropMarks('Exclusion')
+    # apply modifiers to any targets within the modifiers scope
+    markup.applyModifiers()
+    markup.pruneSelfModifyingRelationships()
+    if prune_inactive:
+        markup.dropInactiveModifiers()
+    return markup
+
+def markup_context(rslts):
+    context = pyConText.ConTextDocument()
+    for r in rslts:
+        context.addMarkup(r)
+    return context
